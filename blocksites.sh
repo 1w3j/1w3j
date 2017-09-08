9b64a4c1239140850be77ab89db11bb45599ee7b
iqbal98 <iqbal_cs98@hotmail.com>
2017-09-08 20:44:01 -0300

    2.5

---
+++ blocksites.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+hosts="/etc/hosts"
+
+echo "The following entries are going to be appended to /etc/hosts in order to block them all:\n"
+for url in $*; do
+    echo -e "\t$url"
+done;
+
+echo -e "\nModifying /etc/hosts..."
+
+for url in $*; do
+    entry="127.0.0.1\t$url"
+    echo -e "\t(*) Appending \"$entry\"..."
+    echo -e "$entry" | tee -a /etc/hosts 1>/dev/null
+done;
