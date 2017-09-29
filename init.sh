a536b639cae2a2b5e4fb58df500a06da4fbcd68c
iqbal98 <iqbal_cs98@hotmail.com>
2017-09-29 18:11:55 -0300

    2.7

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-REPO_PATH=~/1w3j;
+SCRIPTS_PATH=~/1w3j/scripts;
+CONFIGS_PATH=~/1w3j/conf
 BIN_PATH=~/bin;
 CONFIG_FILES=(bashrc gvimrc ideavimrc tmux.conf vimrc vimrc.after vimrc.before zshrc)
 
@@ -8,12 +9,12 @@ link_scripts() {
     echo -e "\033[32m(*) Found these .$1 scripts:\033[m";
     FILES=();
     # globbing files with he specified extension
-    for f in $REPO_PATH/*.$1; do
+    for f in $SCRIPTS_PATH/*.$1; do
         # check if current $f file is actually a file (not dirs) while excluding this script file
         [ -f "$f" ] && [ "$f" != "$(realpath $0)" ] && FIEL=$(basename $f);
         # note: shortened filenames just for fooling around
-        # concatenating the REPO_PATH "path string", a "/" slash, and the current file string "globbed"
-        FILE=$REPO_PATH/$FIEL;
+        # concatenating the SCRIPTS_PATH "path string", a "/" slash, and the current file string "globbed"
+        FILE=$SCRIPTS_PATH/$FIEL;
         echo -e "\t$FILE";
         # append that string to the FILES array
         FILES=("$FILE" "${FILES[@]}");
@@ -37,11 +38,11 @@ link_scripts() {
 link_config_files() {
     echo -e "\033[32m(*)Found these config files\033[m";
     for c in "${CONFIG_FILES[@]}"; do
-        echo -e "\t$REPO_PATH/$c";
+        echo -e "\t$CONFIGS_PATH/$c";
     done;
     echo -e "\033[32mStarting the link process:\033[m";
     for c in "${CONFIG_FILES[@]}"; do
-        from=$REPO_PATH/$c;
+        from=$CONFIGS_PATH/$c;
 	to=~/.$c;
         echo -e "linking \033[31m$from\033[m to \033[31m$to\033[m";
         rm -f $to;
