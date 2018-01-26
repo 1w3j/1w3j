ffacef20f0538435cefd6a5da2bf06239af35521
iqbal98 <iqbal_cs98@hotmail.com>
2018-01-26 12:49:57 -0500

    3.5

--- scripts/cptomine.sh
+++ scripts/cptomine.sh
@@ -4,12 +4,13 @@ MINE_FOLDER=~/mine;
 RM_CHAR=":";
 
 if [ -n "$*" ]; then
-  for file in $*; do
+  #note $@ does not care of $IFS, $* does
+  for file in "$@"; do
     if [ -e "$file" ]; then
       NEW_NAME=`basename "$file" | tr --delete "$RM_CHAR"`;
       NEW_PATH=$MINE_FOLDER/$NEW_NAME;
-      echo "(*) Copying $file to $NEW_PATH";
-      cp "$file" "$NEW_PATH";
+      echo "(*) Copying $file ==> $NEW_PATH";
+      cp -L "$file" "$NEW_PATH";
     else
       >&2 echo 3rr0r: "$file": File doesn\'t exist;
     fi;
