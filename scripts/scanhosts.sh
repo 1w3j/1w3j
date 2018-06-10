776a2e82bc90845295dfa8aefb22c17cd01679ab
iqbal <iqbal_cs98@hotmail.com>
2018-06-10 01:01:10 -0500

    4.3.4

--- scripts/scanhosts.sh
+++ scripts/scanhosts.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env sh
 
-for ip in $(seq 1 254); do ping -c 1 192.168.0.$ip>/dev/null; [ $? -eq 0 ] && echo "192.168.1.$ip UP" || : ; done
+# Usage:
+#         scanhosts [number of the subnet]
+# Example:
+#         $ scanhosts 0   # pings all 192.168.0.* IPs
+#         $ scanhosts 43   # pings all 192.168.43.* IPs
+
+for ip in $(seq 1 254); do ping -c 1 192.168."$1".$ip>/dev/null; [ $? -eq 0 ] && echo 192.168."$1".$ip UP || : ; done
