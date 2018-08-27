7759beaad92b0c5ac46e7ef562dce298c89642b0
iqbal <iqbal_cs98@hotmail.com>
2018-08-26 21:53:48 -0500

    4.3.8

--- scripts/topwords.sh
+++ scripts/topwords.sh
@@ -6,7 +6,10 @@ FOLDER=${1};
 # no indentation
 EXCLUDED_WORDS="pdf\|zip\|epub\|azw3\|azw4\|chm\|video\|1st\|3rd\|2nd\|s\|First\|Second\|third\|fourth\|technologies\|edition\|practical\|hands\|cookbook\|guide\|application\|applications\|app\|stop\|apps\
 \|using\|build\|learning\|introduction\|user\|users\|skills\|theoretical\|theory\|yourself\|concepts\|beginner\|beginners\|start\|own\|click\|like\|mine\|make\|techniques\|technique\|beginning\|begin\
-\|fifth\|sixth\|eight\|ninth\|tenth\|eleventh\|twelfth\|thirteenth\|fourteenth\|fifteenth\|4th\|5th\|6th\|7th\|8th\|9th\|10th\|11th\|12th\|13th\|14th\|15th\|manage\|student\|students"
+\|fifth\|sixth\|eight\|ninth\|tenth\|eleventh\|twelfth\|thirteenth\|fourteenth\|fifteenth\|4th\|5th\|6th\|7th\|8th\|9th\|10th\|11th\|12th\|13th\|14th\|15th\|manage\|student\|students\|implement\|implementation\
+\|making\|create\|using\|driven\|drive\|straight\|fascinating\|fantastic\|awesome\|bundle\|books\|book\|pc\|1\|2\|3\|4\|5\|6\|7\|8\|9\|10\|11\|12\|13\|14\|15\|16\|17\|18\|19\|20\|handbook\|practices\|practice\
+\|volume\|best\|engineers\|engineer\|top\|set\|power\|server\|press\|recipes\|reference\|learn\|technology\|tech\|development\|programming\|solutions\|information\|course\|integrated\|intermediate\|basic\|website\
+\|advanced\|basic\|visual\|version\|power\|stack\|startup\|side\|secure\|training\|monitoring\|scope\|scalable\|sectors\|sector\|scratch
 
 topwords() {
     if [[ -n "${FOLDER}" ]]; then
