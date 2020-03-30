b2f56a0ee10820a52a16e1a6962799f22b9d7ff3
申杰 <iqbal_cs98@hotmail.com>
2020-03-30 22:30:31 +0800

    4.6.1

--- scripts/modprobe_vmware.sh
+++ scripts/modprobe_vmware.sh
@@ -1,2 +1,23 @@
 #!/usr/bin/env sh
+
+# How to install VMware Workstation 15.5.1 on Arch Linux
+# - Install linux-headers with the same version as system kernel
+#  version (uname -r)
+# - Read and follow the Arch Linux Guide for VMware
+# - Change the /etc/init.d/vmware, the global variable mod=vmw_vmci,
+#  search for the 'vmwareStartVmci' function, edit vmci=vmw_vmci
+# - tar -xzf workstation-15.5.1.tar.gz
+# - cd vmware-host-modules-workstation-15.5.1
+# - make
+# - sudo make install
+# 
+# if for some reason still isn't completed
+# - Install vmware-patch from AUR
+# - Run sudo vmware-patch -fvk
+# - Run sudo vmware-modconfig --console --install-all
+# - Run this script
+# - sudo /etc/init.d/vmware start
+
 modprobe vmw_vmci
+modprobe vmmon
+modprobe vmnet
--- scripts/resetintellijkey.sh
+++ scripts/resetintellijkey.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Usage: resetintellijkey 'idea' && resetintellijkey 'clion' && resetintellijkey 'goland'
+
 source ~/1w3j/functions.sh;
 
 PYCHARM=charm; #usage => $ resetintellijkey charm
@@ -25,13 +27,13 @@ reset_keys(){
     IDE_CONFIG=${2};
 
     msg "Removing the evaluation keys";
-    rm  -rf ${IDE_CONFIG}/config/eval;
+    rm  -rf "${IDE_CONFIG}/config/eval";
 
-    rm -rf ~/.java/.userPrefs/jetbrains/pycharm;
+    rm -rf "~/.java/.userPrefs/jetbrains/${IDE}";
 
     msg "Resetting evalsprt in options.xml";
     # 2018 versions : change other.xml to options.xml
-    sed -i '/evlsprt/d' ${IDE_CONFIG}/config/options/other.xml;
+    sed -i '/evlsprt/d' "${IDE_CONFIG}/config/options/other.xml";
 
     msg "Resetting evalsprt in prefs.xml";
     sed -i '/evlsprt/d' ~/.java/.userPrefs/prefs.xml;
@@ -40,7 +42,7 @@ reset_keys(){
     rm -f "${IDE_CONFIG}/config/options/eval";
 
     msg "Deleting ${IDE} user prefs folder";
-    rm -rf ~/.java/userPrefs/jetbrains/${IDE};
+    rm -rf "~/.java/userPrefs/jetbrains/${IDE}";
 
     msg "Touching files";
     find ${IDE_CONFIG} -type d -exec touch -t $(date +"%Y%m%d%H%M") {} +;
@@ -72,8 +74,27 @@ handle_configpath_params(){
     esac;
 }
 
+print_usage() {
+    case "$(basename ${0})" in
+        resetintellijkey|resetintellijkey.sh)
+            cat <<EOF
+Usage: ${0} {IDE} [--just-get-configpath {IDE}]
+Options:
+        {IDE}                                     The IDE name as in the scripts created by the
+                                                Jetbrains Toolbox app, must be lowercase, and only
+                                                one name per runtime
+        --just-get-configpath                     Only output the config folder of IDE
+EOF
+        ;;
+    esac
+}
+
 resetintellijkey(){
     case "${1}" in
+        -h)
+            ;;
+        --help)
+            ;;
         ${PYCHARM})
             IDE=${PYCHARM};
             ;;
@@ -103,12 +124,21 @@ resetintellijkey(){
     IDE_CONFIG=`eval echo '$'"${UPPER_IDE}_CONFIG"`;
     IDE_CONFIG=$(ls -td ${IDE_CONFIG}* | head -1 | tr ':' ' ');
 
-    msg ${IDE} detected;
-    msg ${IDE_CONFIG} detected;
-
-    if [[ ! "${1}" = "--just-get-configpath" ]]; then
-        reset_keys ${IDE} ${IDE_CONFIG};
-    fi
+    case "${1}" in
+        --help|-h)
+            print_usage;
+            ;;
+        --just-get-configpath)
+            msg ${IDE} detected;
+            msg ${IDE_CONFIG} detected;
+            ;;
+        *)
+            msg ${IDE} detected;
+            msg ${IDE_CONFIG} detected;
+            msg resettt
+            #reset_keys ${IDE} ${IDE_CONFIG};
+            ;;
+    esac
 }
 
 resetintellijkey ${*};
