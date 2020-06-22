392b92cc2042258af8a69f388050fcb8cce929d8
1w3j <iqbal_cs98@hotmail.com>
2020-06-22 16:32:54 +0800

    4.9.8

--- init.sh
+++ init.sh
@@ -261,12 +261,12 @@ print_usage() {
 
 Usage: ${0##*/} [--do-not-install-anything | -dnia ]
 Options:
-        --do-not-install-anything, -dnia            Just link your config files without installing packages listed in \
+        --do-not-install-anything, -dnia            Just link your config files without installing the packages listed in \
 ~/1w3j/pkgnames
         -rc,--rc,--reload-config-files              Link dotfiles, themes, color schemes, etc
         -rs,--rs,--reload-scripts                   Link scripts/* files to ~/bin
         -uti,--uti,--upload-themes-icons            Compress all theme and icon folders located at GTK_THEMES_PATH and \
-GTK_ICONS_PATH respectively preparing it for repo updates
+GTK_ICONS_PATH respectively, preparing it for repo updates
 EOF
         ;;
     esac
